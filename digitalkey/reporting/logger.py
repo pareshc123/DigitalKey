66b695bf9bf556d1648006b757d10923f79911e1
Paresh Choudhary <pareshc123@gmail.com>
2026-04-03 12:34:49 +0200

    Feature/logger/PaCh: added colour format for console level logging

--- digitalkey/reporting/logger.py
+++ digitalkey/reporting/logger.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import logging
 from pathlib import Path
 from datetime import datetime
@@ -27,6 +28,40 @@ class MicrosecondFormatter(logging.Formatter):
 
 LOG_FORMAT = "[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s"
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
+
+
+class ColorFormatter(logging.Formatter):
+
+    COLORS = {
+        "DEBUG": "\033[36m",            # Cyan
+        "INFO": "\033[32m",             # Green
+        "WARNING": "\033[33m",          # Yellow
+        "ERROR": "\033[31m",            # Red
+        "CRITICAL": "\033[41m",         # Red background
+    }
+    RESET = "\033[0m"
+
+    def formatTime(self, record, datefmt=None):
+        dt = datetime.fromtimestamp(record.created)
+        if datefmt:
+            return dt.strftime(datefmt)
+        return dt.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+    def format(self, record):
+        # Save original levelname
+        original_levelname = record.levelname
+
+        # Apply color ONLY to levelname
+        color = self.COLORS.get(record.levelname, self.RESET)
+        record.levelname = f"{color}{record.levelname}{self.RESET}"
+
+        # Format the message
+        message = super().format(record)
+
+        # Restore original levelname (VERY IMPORTANT)
+        record.levelname = original_levelname
+
+        return message
 
 
 def _setup_logger(log_level: str = "INFO") -> None:
@@ -62,8 +97,8 @@ def _setup_logger(log_level: str = "INFO") -> None:
     )
 
     # Console Handler
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(ColorFormatter(LOG_FORMAT, DATE_FORMAT))
 
     # File Handler with rotation (daily)
     file_handler = TimedRotatingFileHandler(
@@ -89,3 +124,22 @@ def get_logger(name: str) -> logging.Logger:
     """
 
     return logging.getLogger(name)
+
+
+if __name__ == "__main__":
+
+    _setup_logger("DEBUG")
+    lg = get_logger(__name__)
+
+    lg.info("INFO level")
+    time.sleep(1)
+    lg.debug("DEBUG level")
+    time.sleep(1.25)
+
+    lg.critical("CRITICAL level")
+    time.sleep(0.25)
+
+    lg.warning("WARN level")
+    time.sleep(2.55)
+
+    lg.error("ERROR level")
