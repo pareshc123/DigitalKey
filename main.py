eb74e3063fab8aced8041218a18d5e2bf664a3a2
Paresh Choudhary <pareshc123@gmail.com>
2026-04-03 18:33:30 +0200

    reporting/PaCh:integrate logger in main.py

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 import yaml
+from pathlib import Path
 
 from digitalkey.core.log_parser import LogParser
 from digitalkey.validation.validator import Validator
+from digitalkey.reporting.logger import setup_logger, get_logger
 from digitalkey.reporting.report_generator import ReportGenerator
 
 
-# function to load .yaml
-def load_yaml(path: str):
-    with open(path, "r") as file:
-        return yaml.safe_load(file)
+# Utilities
+def load_yaml(path: Path):
+    logger = get_logger(__name__)
+
+    try:
+        with open(path, "r") as file:
+            logger.debug(f"{path.name}.yaml opened successfully.")
+            return yaml.safe_load(file)
+    except Exception as e:
+        logger.exception(f"Failed to open {path.name} file. Exception: ", e)
+        return None
 
 
 def main():
+    # setup logger
+    setup_logger("DEBUG")
+    logger = get_logger(__name__)
 
-    # load config
-    config = load_yaml(r"config/test_config.yaml")
+    logger.info("Starting Digital Key validation framework")
+
+    base_dir = Path(__file__).resolve().parent
+
+    # load config and thresholds
+    logger.info("Loading configuration files")
+
+    config = load_yaml(base_dir / "config/test_config.yaml")
 
     # load threshold
-    thresholds = load_yaml(r"config/thresholds.yaml")
+    thresholds = load_yaml(base_dir / "config/thresholds.yaml")
 
     # parser
     parser = LogParser(r"Traces/success.log")
